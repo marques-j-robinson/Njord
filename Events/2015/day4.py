b4a3b96f9037f588dcb8358dbdcaf7aa6460b166
marques-j-robinson <marques.j.robinson@gmail.com>
2021-04-23 22:18:28 -0700

    solve 2015 day 4

---
+++ Events/2015/day4.py
@@ -0,0 +1,55 @@
+# Psuedo code
+
+# Part 1
+# -
+
+# Part 2
+# -
+
+
+import hashlib
+
+
+class Solution:
+
+    def __init__(self, data):
+        self.data = data
+
+    def run(self):
+        self.part_01()
+        print(self.res)
+        self.part_02()
+        print(self.res)
+
+    def part_01(self):
+        self.res = 0
+        mine = Mine(self.data)
+        self.res = mine.start(5)
+
+    def part_02(self):
+        self.res = 0
+        mine = Mine(self.data)
+        self.res = mine.start(6)
+
+
+class Mine:
+
+    def __init__(self, secret_key):
+        self.secret_key = secret_key
+
+    def start(self, target):
+        zeros = "".join(["0"] * target)
+        count = 0
+        exit = False
+        while exit is False:
+            res = self.extract(count)
+            if res[0:target] == zeros:
+                exit = True
+            else:
+                count += 1
+        return count
+
+    def extract(self, count):
+        str2hash = f"{self.secret_key}{count}"
+        res = hashlib.md5(str2hash.encode())
+        return res.hexdigest()
