a7bc1eaeceaa377dfb91028d416e2b01a6905417
marques-j-robinson <marques.j.robinson@gmail.com>
2021-04-23 22:45:36 -0700

    solve 2015 day 05

---
+++ Events/2015/day5.py
@@ -0,0 +1,83 @@
+# Psuedo code
+
+# Part 1
+# -
+
+# Part 2
+# -
+
+
+class Solution:
+
+    def __init__(self, data):
+        self.data = data.split('\n')
+
+    def run(self):
+        self.part_01()
+        print(self.res)
+        self.part_02()
+        print(self.res)
+
+    def part_01(self):
+        self.res = 0
+        for l in self.data:
+            v = Validation(l)
+            if v.vowel_count() and v.dup_letters() and v.check_forbidden():
+                self.res += 1
+
+    def part_02(self):
+        self.res = 0
+        for l in self.data:
+            v = Validation(l)
+            if v.dup_pairs() and v.repeat_every_other_letter():
+                self.res += 1
+
+
+vowels = 'aeiou'
+forbidden_strings = ['ab', 'cd', 'pq', 'xy']
+
+
+class Validation:
+
+    def __init__(self, val):
+        self.val = val
+
+    def vowel_count(self):
+        count = 0
+        for l in self.val:
+            if l in vowels:
+                count += 1
+        return count >= 3
+
+    def dup_letters(self):
+        res = False
+        for idx, l in enumerate(self.val):
+            if idx > 0:
+                if self.val[idx-1] == l:
+                    res = True
+        return res
+
+    def check_forbidden(self):
+        res = True
+        for s in forbidden_strings:
+            if s in self.val:
+                res = False
+        return res
+
+    def dup_pairs(self):
+        res = False
+        for idx, l in enumerate(self.val):
+            if idx > 0:
+                pair = f"{self.val[idx - 1]}{l}"
+                if pair in self.val[idx + 1:]:
+                    res = True
+        return res
+
+    def repeat_every_other_letter(self):
+        res = False
+        for idx, l in enumerate(self.val):
+            if idx > 1:
+                prev = self.val[idx - 2]
+                if prev == l:
+                    res = True
+        return res
