e521da2bccb45486a71e47efd75951e26bd310a8
marques-j-robinson <marques.j.robinson@gmail.com>
2021-04-24 22:59:20 -0700

    refactor main code

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import os
 import sys
 import importlib
-
 import requests
 from dotenv import load_dotenv
 
+
 load_dotenv()
 SESSION = os.getenv('SESSION')
-
-
-def get_puzzle_input(event, day):
-    uri = f"http://adventofcode.com/{event}/day/{day}/input"
-    res = requests.get(uri, cookies={"session": SESSION})
-    return res.text
 
 
 def is_event(event):
@@ -27,31 +21,13 @@ def is_cache(cache_path):
     return os.path.isfile(cache_path)
 
 
-def main():
-    event = None
-    day = None
-    data = None
+def get_puzzle_input(event, day):
+    uri = f"http://adventofcode.com/{event}/day/{day}/input"
+    res = requests.get(uri, cookies={"session": SESSION})
+    return res.text
 
-    # allow user to provide args, eg: $ python main.py {event} {day}
-    if len(sys.argv) >= 2:
-        event = sys.argv[1]
-        day = sys.argv[2]
 
-    # prompt user for event
-    while event is None:
-        event = input('Event (year): ')
-        if is_event(event) is False:
-            print('Event does not exist... Try again.')
-            event = None
-
-    # prompt user for day
-    while day is None:
-        day = input('Day: ')
-        if is_day(event, day) is False:
-            print('Day does not exist... Try again.')
-            day = None
-
-    # cache layer
+def cache_layer(event, day):
     if os.path.isdir('cache') is False:
         os.mkdir('cache')
     cache_path = f'cache/{event}_{day}'
@@ -65,14 +41,46 @@ def main():
         f = open(cache_path, 'a')
         f.write(data)
         f.close()
+    return data
 
-    # get module and execute solution
+
+def user_input():
+    event = None
+    day = None
+    # allow args eg: $ python main.py {event} {day}
+    if len(sys.argv) >= 2:
+        event = sys.argv[1]
+        day = sys.argv[2]
+    else:
+        # prompt for event
+        while event is None:
+            event = input('Event (year): ')
+            if is_event(event) is False:
+                print('Event does not exist... Try again.')
+                event = None
+
+        # prompt for day
+        while day is None:
+            day = input('Day: ')
+            if is_day(event, day) is False:
+                print('Day does not exist... Try again.')
+                day = None
+    return [event, day]
+
+
+def solve(event, day, data):
     module = importlib.import_module(f"Events.{event}.day{day}")
-    solution = module.Solution(data.strip())
-    solution.part_01()
-    print(solution.p1)
-    solution.part_02()
-    print(solution.p2)
+    s = module.Solution(data.strip())
+    s.part_01()
+    print(s.p1)
+    s.part_02()
+    print(s.p2)
+
+
+def main():
+    [event, day] = user_input()
+    data = cache_layer(event, day)
+    solve(event, day, data)
 
 
 if __name__ == "__main__":
