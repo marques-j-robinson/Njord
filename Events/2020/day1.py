6f873e638eb8ff41a8b0df6368a16f7772dca729
marques-j-robinson <marques.j.robinson@gmail.com>
2021-04-24 22:18:12 -0700

    solve 2020 day 01

---
+++ Events/2020/day1.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def __init__(self, data):
+        self.data = [int(l) for l in data.split()]
+        self.p1 = 0
+        self.p2 = 0
+
+    def part_01(self):
+        for i in self.data:
+            for j in self.data:
+                if i+j == 2020:
+                    self.p1 = i*j
+
+    def part_02(self):
+        for i in self.data:
+            for j in self.data:
+                for k in self.data:
+                    if i+j+k == 2020:
+                        self.p2 = i*j*k
