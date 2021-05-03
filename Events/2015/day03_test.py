541393b68990f65107b32f7fc0e4fed9e2ca0265
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-03 13:28:08 -0700

    update tests for 2015 day 03

--- Events/2015/day03_test.py
+++ Events/2015/day03_test.py
@@ -2,38 +2,35 @@ import unittest
 from day03 import Solution
 
 
+s = Solution()
+
+
 class TestSolution201503(unittest.TestCase):
 
     def test_part_01(self):
-        data = ">"
-        s = Solution(data)
-        s.part_01()
+        s.data = ">"
+        s.solve(1)
         self.assertEqual(s.p1, 2)
 
-        data = "^>v<"
-        s = Solution(data)
-        s.part_01()
+        s.data = "^>v<"
+        s.solve(1)
         self.assertEqual(s.p1, 4)
 
-        data = "^v^v^v^v^v"
-        s = Solution(data)
-        s.part_01()
+        s.data = "^v^v^v^v^v"
+        s.solve(1)
         self.assertEqual(s.p1, 2)
 
     def test_part_02(self):
-        data = "^v"
-        s = Solution(data)
-        s.part_02()
+        s.data = "^v"
+        s.solve(2)
         self.assertEqual(s.p2, 3)
 
-        data = "^>v<"
-        s = Solution(data)
-        s.part_02()
+        s.data = "^>v<"
+        s.solve(2)
         self.assertEqual(s.p2, 3)
 
-        data = "^v^v^v^v^v"
-        s = Solution(data)
-        s.part_02()
+        s.data = "^v^v^v^v^v"
+        s.solve(2)
         self.assertEqual(s.p2, 11)
 
 
