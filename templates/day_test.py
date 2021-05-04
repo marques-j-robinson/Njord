914d66a4d7978ebe3c94db5662997dff626f4a35
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-04 08:49:30 -0700

    update test template to new structure

--- templates/day_test.py
+++ templates/day_test.py
@@ -2,18 +2,19 @@ import unittest
 # from day{day} import Solution
 
 
+# s = Solution()
+
+
 # class TestSolution{event}{day}(unittest.TestCase):
 
     # def test_part_01(self):
-        # data = ""
-        # s = Solution(data)
-        # s.part_01()
+        # s.data = ""
+        # s.solve(1)
         # self.assertEqual(s.p1, 0)
 
     # def test_part_02(self):
-        # data = ""
-        # s = Solution(data)
-        # s.part_02()
+        # s.data = ""
+        # s.solve(2)
         # self.assertEqual(s.p2, 0)
 
 
