0c155d4ba33125e5608a063251fb5395f3900070
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-05 08:21:24 -0700

    update test for 2015 day 05

--- Events/2015/day05_test.py
+++ Events/2015/day05_test.py
@@ -2,53 +2,47 @@ import unittest
 from day05 import Solution
 
 
+s = Solution()
+
+
 class TestSolution201505(unittest.TestCase):
 
     def test_part_01(self):
-        data = "ugknbfddgicrmopn"
-        s = Solution(data)
-        s.part_01()
+        s.data = "ugknbfddgicrmopn"
+        s.solve(1)
         self.assertEqual(s.p1, 1)
 
-        data = "aaa"
-        s = Solution(data)
-        s.part_01()
+        s.data = "aaa"
+        s.solve(1)
         self.assertEqual(s.p1, 1)
 
-        data = "jchzalrnumimnmhp"
-        s = Solution(data)
-        s.part_01()
+        s.data = "jchzalrnumimnmhp"
+        s.solve(1)
         self.assertEqual(s.p1, 0)
 
-        data = "haegwjzuvuyypxyu"
-        s = Solution(data)
-        s.part_01()
+        s.data = "haegwjzuvuyypxyu"
+        s.solve(1)
         self.assertEqual(s.p1, 0)
 
-        data = "dvszwmarrgswjxmb"
-        s = Solution(data)
-        s.part_01()
+        s.data = "dvszwmarrgswjxmb"
+        s.solve(1)
         self.assertEqual(s.p1, 0)
 
     def test_part_02(self):
-        data = "qjhvhtzxzqqjkmpb"
-        s = Solution(data)
-        s.part_02()
+        s.data = "qjhvhtzxzqqjkmpb"
+        s.solve(2)
         self.assertEqual(s.p2, 1)
 
-        data = "xxyxx"
-        s = Solution(data)
-        s.part_02()
+        s.data = "xxyxx"
+        s.solve(2)
         self.assertEqual(s.p2, 1)
 
-        data = "uurcxstgmygtbstg"
-        s = Solution(data)
-        s.part_02()
+        s.data = "uurcxstgmygtbstg"
+        s.solve(2)
         self.assertEqual(s.p2, 0)
 
-        data = "ieodomkazucvgmuy"
-        s = Solution(data)
-        s.part_02()
+        s.data = "ieodomkazucvgmuy"
+        s.solve(2)
         self.assertEqual(s.p2, 0)
 
 
