0d528bec670cb29acdc75fe574e5709d6e024492
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 13:05:10 -0700

    fix main

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import importlib
 from util import UserInput, leading_zero
 
 
-print('hi')
-
-
 def main():
     x = UserInput()
     module = importlib.import_module(f"Events.{x.event}.day{leading_zero(x.day)}")
