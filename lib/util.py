1c3bf29e2ff645d00c74c5385f82ed85c1f685d9
marques-j-robinson <marques.j.robinson@gmail.com>
2021-07-07 14:04:56 -0700

    Refactor

---
+++ lib/util.py
@@ -0,0 +1,51 @@
+import os
+import pyperclip
+from dotenv import load_dotenv
+
+
+load_dotenv()
+SESSION = os.getenv('SESSION')
+
+
+def copy_results(p1, p2):
+    "Automatic copy of results to clipboard"
+    print(p1)
+    if p2 is None or p2 == 0:
+        pyperclip.copy(str(p1))
+    else:
+        print(p2)
+        pyperclip.copy(str(p2))
+
+
+class PuzzleInput:
+
+    def __init__(self, year, day):
+        self.cache_path = f'cache/{year}_{day}'
+        self.req_path = f'http://adventofcode.com/{year}/day/{day}/input'
+
+    def get_puzzle_input(self):
+        if self.is_cached():
+            return self.read_cache()
+        else:
+            return self.fetch()
+
+    def is_cached(self):
+        return os.path.isfile(self.cache_path) is True
+
+    def fetch(self):
+        response = requests.get(self.req_path, cookies={'session': SESSION})
+        puzzle_input = response.text
+        self.write_cache(puzzle_input)
+        return self.format(puzzle_input)
+
+    def write_cache(self, puzzle_input):
+        f = open(self.cache_path, 'a')
+        f.write(puzzle_input)
+        f.close()
+
+    def read_cache(self):
+        f = open(self.cache_path, 'r')
+        return self.format(f.read())
+
+    def format(self, puzzle_input):
+        return puzzle_input.strip()
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+import argparse
 import importlib
-from util import UserInput, leading_zero
+from lib.util import copy_results
 
 
-def main():
-    user_input = UserInput()
-    event = user_input.event
-    day = user_input.day
+def get_args():
+    parser = parser_setup()
+    args = parser.parse_args()
+    year = args.year
+    day = args.day
+    if year is None or day is None:
+        raise Exception('MISSING_REQUIRED_ARGUMENTS')
+    return [year, day]
 
-    module = importlib.import_module(f"Events.{event}.day{leading_zero(day)}")
-    s = module.Solution(event, day)
+
+def parser_setup():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-y', '--year')
+    parser.add_argument('-d', '--day')
+    return parser
+
+
+def solve(year, day):
+    module = importlib.import_module(f'Solutions.{year}.day{day}')
+    s = modlue.Solution(year, day)
     s.solve()
-    s.copy_to_clipboard()
+    copy_results(s.p1, s.p2)
 
 
 if __name__ == "__main__":
-    main()
+    [year, day] = get_args()
+    solve(year, day)
