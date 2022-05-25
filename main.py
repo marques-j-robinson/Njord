e9c3eaf3fd5ff8d7c6e28f78371c26699ac8017c
marques-j-robinson <marques.j.robinson@gmail.com>
2022-05-24 22:05:40 -0700

    remove unused function

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def solve(year, day):
     module = importlib.import_module(f'Solutions.{year}.day{day}')
     s = module.Solution(year, day)
     s.solve()
-    copy_results(s.p1, s.p2)
 
 
 if __name__ == "__main__":
