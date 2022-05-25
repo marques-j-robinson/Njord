54c8976d910760d26e2d93887d7b7640b646ef8f
marques-j-robinson <marques.j.robinson@gmail.com>
2022-05-24 21:33:57 -0700

    change env var names and setup user input to read puzzle id from env and then from the args

--- input_request/user_input.py
+++ input_request/user_input.py
@@ -1,18 +1,29 @@
+import os
 import argparse
+from dotenv import load_dotenv
 
 
 class UserInput:
 
     def __init__(self):
-        parser = self.parser_setup()
-        args = parser.parse_args()
+        self.read_puzzle_id_from_env()
+
+    def read_puzzle_id_from_env(self):
+        load_dotenv()
+        self.year = os.getenv('YEAR')
+        self.day = os.getenv('DAY')
+        if self.year is None or self.day is None:
+            self.get_puzzle_id_from_argparse()
+    
+    def get_puzzle_id_from_argparse(self):
+        self.create_parser()
+        args = self.parser.parse_args()
         self.year = args.year
         self.day = args.day
         if self.year is None or self.day is None:
             raise Exception('MISSING_REQUIRED_ARGUMENTS')
 
-    def parser_setup(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-y', '--year')
-        parser.add_argument('-d', '--day')
-        return parser
+    def create_parser(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('-y', '--year')
+        self.parser.add_argument('-d', '--day')
